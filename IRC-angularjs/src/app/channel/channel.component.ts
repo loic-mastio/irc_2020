829c72a3e65d1b6db07fc2388364dcd5dc770257
Loïc Mastio <loic.mastio@epitech.eu>
2020-04-08 22:50:00 +0200

    create_channel_done_id_user_to_do

--- IRC-angularjs/src/app/channel/channel.component.ts
+++ IRC-angularjs/src/app/channel/channel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreateChannel } from '../../entity/createchannels';
 
 @Component({
   selector: 'app-channel',
@@ -6,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel.component.css']
 })
 export class ChannelComponent implements OnInit {
+  submitted = false;
+  channel = new CreateChannel('NomDuchanel', 0, 1, 1);
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.channel);
+    //Submitting the form (or model) data to REST endpoint
+    this.http.post('http://localhost:3000/channels/create', this.channel)
+      .subscribe(
+        (res: CreateChannel) => {console.log(res.name);}  // display response data
+      );
+  }
   ngOnInit(): void {
   }
 
---
+++ IRC-angularjs/src/entity/createchannels.ts
@@ -0,0 +1,10 @@
+export class CreateChannel {
+  constructor(
+  public name: string,
+  public nbr_user_max: number,
+  public nbr_user: number,
+  public owner_id: number
+){
+
+}
+}
