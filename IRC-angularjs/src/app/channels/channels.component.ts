5413a61d970c48ec0169960174e18942806c14a2
Loïc Mastio <loic.mastio@epitech.eu>
2020-04-09 07:59:54 +0200

    getAllChanID

--- IRC-angularjs/src/app/channels/channels.component.ts
+++ IRC-angularjs/src/app/channels/channels.component.ts
@@ -1,27 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { UserService } from '../user.service';
+import {HttpClient} from "@angular/common/http";
+import { ChannelLiked } from "../../entity/channelliked";
 
 @Component({
   selector: 'app-channels',
   templateUrl: './channels.component.html',
   styleUrls: ['./channels.component.css']
 })
+
+
+
 export class ChannelsComponent implements OnInit {
   userService: UserService;
   message = '';
   messages = [];
+  channels = [];
 
-  constructor(private chatService: ChatService, userService: UserService) { 
+  constructor(private chatService: ChatService, userService: UserService, private http: HttpClient) {
     this.userService = userService;
     this.chatService
     .listen('message')
     .subscribe(message => {
       this.messages.push(message);
     })
+    this.channels.push(this.http.get('http://localhost:3000/channels/getbyOwnerId/' + this.userService.id));
+    console.log('channels' + this.channels);
+  }
+
+  fetch_channels() {
+    this.http.get('http://localhost:3000/channelsFav/liked/' + this.userService.id)
+      .subscribe(
+        (res: ChannelLiked) => {
+          this.channels.push(res);
+          this.channels.shift();
+          console.log(this.channels);
+        }  // display response data
+      );
   }
 
   ngOnInit(): void {
+    this.fetch_channels();
   }
 
   sendMessage() {
---
+++ IRC-angularjs/src/entity/channelliked.ts
@@ -0,0 +1,6 @@
+export class ChannelLiked {
+  constructor(public id: number,
+              public id_channels: number,
+              public id_user: number
+  ){}
+}
