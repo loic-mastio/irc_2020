3ffb2cb5c4cfae3838248c043e49b06a19743116
pierre harelle <harelle@unistra.fr>
2020-04-09 06:16:22 +0200

    Prepar for merge

--- IRC-angularjs/src/app/login/login.component.ts
+++ IRC-angularjs/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userService: UserService;
 
-  constructor() { }
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
 
   ngOnInit(): void {
   }
 
+  setUserName(name: string) {
+    this.userService.setName(name);
+  }
 }
